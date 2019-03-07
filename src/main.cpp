dcec261d203ae3fa9090a9d3c36893a2cea232d3
Wojciech Celej <wojciech1871@gmail.com>
2019-03-07 14:39:00 +0100

    txt parser

--- src/main.cpp
+++ src/main.cpp
@@ -7,49 +7,17 @@
 #include "Vertex.h"
 #include "MyAlgorithm.h"
 
-using namespace std;
-
+void txtFileParser(std::vector<Point>& points) {
+    int number, weight;
+    double x, y;
+    while (std::cin >>number >>x >>y >>weight) {
+        points.emplace_back(Point(number, x, y, weight));
+    }
+}
 int main() {
-    cout <<"Points" <<endl;
-    Point p1 = Point(1, 10, 10, 15);
-    Point p2 = Point(2, 8, 2, 10);
-    Point p3 = Point(3, 7, 4, 11);
-    Point p4 = Point(4, 4, 1, 4);
-    Point p5 = Point(5, 5, 11, 30);
-    cout <<p1 <<endl;
-    cout <<p2 <<endl;
-    cout <<p3 <<endl;
-    cout <<p4 <<endl;
-    cout <<p5 <<endl;
-
-    vector<Point> points;
-    MyAlgorithm myAg = MyAlgorithm();
-
-    points.push_back(p1);
-    points.push_back(p2);
-    points.push_back(p3);
-    points.push_back(p4);
-    points.push_back(p5);
-
-    myAg.runAlgorithmLSLamana(points);
-//
-//        cout <<"After sorted" <<endl;
-//        for(auto it = points.rbegin(); it != points.rend(); it++) {
-//            cout <<*it <<endl;
-//        }
-//
-//        pointsMap[p1.getY()] = new Vertex(&p1);
-//        pointsMap[p2.getY()] = new Vertex(&p2);
-//        pointsMap[p3.getY()] = new Vertex(&p3);
-//        pointsMap[p4.getY()] = new Vertex(&p4);
-//
-//        cout <<"Map by y" <<endl;
-//        for(auto it = pointsMap.begin(); it != pointsMap.end(); it++) {
-//
-//        }
-//
-//        myAg.printPoints();
-    int result = myAg.returnAlgorithmResult();
-    cout <<"alg result is " <<result <<endl;
-    myAg.printVertexPath();
+    std::vector<Point> pointsVector;
+    MyAlgorithm myAlgorithm;
+    txtFileParser(pointsVector);
+    myAlgorithm.runAlgorithmLSLamana(pointsVector);
+    std::cout <<"Result: " <<myAlgorithm.returnAlgorithmResult() <<std::endl;
 }
---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+POINTS_PATH=pointsFiles
+EXECUTABLE_PATH=cmake-build-debug/src
+
+for file in ${POINTS_PATH}/*
+
+do
+	echo File ${file} results:
+	./${EXECUTABLE_PATH}/prog < ${file}
+done
